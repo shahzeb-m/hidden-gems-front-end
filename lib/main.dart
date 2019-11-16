02bb1d8041eded16b79cc2fe19f79e1a22ed2ddd
Shahzeb Manjlai <shahzeb.manjlai@gmail.com>
2019-11-16 08:07:11 +0000

    log in UI and sign up UI fully implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_multiselect/flutter_multiselect.dart';
 
 void main() => runApp(MyApp());
 
@@ -95,6 +97,28 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
 
+    final registerButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.push(
+            context,
+            new MaterialPageRoute(builder: (context) => new MySignUpPage()),
+          );
+        },
+        child: Text("Register",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+
+
     return Scaffold(
       body: Center(
           child: Container(
@@ -118,6 +142,119 @@ class _MyHomePageState extends State<MyHomePage> {
               passwordField,
               SizedBox(height: 35.0),
               loginButton,
+              SizedBox(height: 25.0),
+              registerButton,
+              SizedBox(height: 15.0),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
+
+// INTERESTS
+// INTERESTS
+// INTERESTS
+
+class MyInterestsPage extends StatefulWidget {
+  MyInterestsPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyInterestsPage createState() => _MyInterestsPage();
+}
+
+class _MyInterestsPage extends State<MyInterestsPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  final String title = "Interests";
+
+  @override
+  Widget build(BuildContext context) {
+    final interests = MultiSelect(
+        autovalidate: false,
+        titleText: title,
+        validator: (value) {
+          if (value == null) {
+            return 'Please select one or more option(s)';
+          }
+        },
+        errorText: 'Please select one or more option(s)',
+        dataSource: [
+          {
+            "display": "Food",
+            "value": 1,
+          },
+          {
+            "display": "Culture",
+            "value": 2,
+          },
+          {
+            "display": "Art",
+            "value": 3,
+          },
+          {
+            "display": "Adventure",
+            "value": 4,
+          },
+          {
+            "display": "Nature",
+            "value": 5,
+          },
+          {
+            "display": "Social",
+            "value": 6,
+          }
+        ],
+        textField: 'display',
+        valueField: 'value',
+        filterable: true,
+        required: true,
+        value: null,
+        onSaved: (value) {
+          // do POST stuff here
+          print('The value is $value');
+        });
+
+    final backButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text("Back",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      body: Center(
+          child: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 155.0,
+                child: Image.asset(
+                  "assets/hiddengem.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              SizedBox(height: 45.0),
+              interests,
+              SizedBox(height: 25.0),
+              backButton,
               SizedBox(height: 15.0),
             ],
           ),
@@ -152,7 +289,21 @@ class MySignUpPage extends StatefulWidget {
 }
 
 class _MySignUpPageState extends State<MySignUpPage> {
+  String dropdownValue = 'One';
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  DateTime selectedDate = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(1950),
+        lastDate: DateTime(DateTime.now().year + 1));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -162,6 +313,26 @@ class _MySignUpPageState extends State<MySignUpPage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    final firstNameField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "First Name",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final lastNameField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Last Name",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
 
     final emailField = TextField(
       obscureText: false,
@@ -183,7 +354,17 @@ class _MySignUpPageState extends State<MySignUpPage> {
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
 
-    final loginButton = Material(
+    final confirmField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Re-enter password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final registerButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
       color: Color(0xff01A0C7),
@@ -193,7 +374,61 @@ class _MySignUpPageState extends State<MySignUpPage> {
         onPressed: () {
           Navigator.pop(context);
         },
-        child: Text("Login",
+        child: Text("Register",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final backButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text("Back",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final interestsButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          Navigator.push(
+            context,
+            new MaterialPageRoute(builder: (context) => new MyInterestsPage()),
+          );
+        },
+        child: Text("Interests",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final dateButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(32.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          _selectDate(context);
+        },
+        child: Text("Select DOB",
             textAlign: TextAlign.center,
             style: style.copyWith(
                 color: Colors.white, fontWeight: FontWeight.bold)),
@@ -209,21 +444,28 @@ class _MySignUpPageState extends State<MySignUpPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               SizedBox(
-                height: 155.0,
-                child: Image.asset(
-                  "assets/logo.png",
-                  fit: BoxFit.contain,
-                ),
+                height: 20.0,
               ),
-              SizedBox(height: 45.0),
-              emailField,
-              SizedBox(height: 25.0),
-              passwordField,
-              SizedBox(height: 35.0),
-              loginButton,
+              firstNameField,
               SizedBox(height: 15.0),
+              lastNameField,
+              SizedBox(height: 15.0),
+              emailField,
+              SizedBox(height: 15.0),
+              passwordField,
+              SizedBox(height: 15.0),
+              confirmField,
+              SizedBox(height: 15.0),
+              dateButton,
+              SizedBox(height: 15.0),
+              interestsButton,
+              SizedBox(height: 15.0),
+              registerButton,
+              SizedBox(height: 25.0),
+              backButton,
             ],
           ),
         ),
